aace1f1c805a42a298d2a67ada87b51508756420
Jonas Kopka <jonas.kopka@gmail.com>
2019-05-26 17:51:59 +0200

    Small corrections

--- i-gem-wiki/src/web/pages/Team.js
+++ i-gem-wiki/src/web/pages/Team.js
@@ -4,16 +4,7 @@ class Team extends Component {
   render() {
     return (
       <div>
-        <h2>Team</h2>
-        <p>Mauris sem velit, vehicula eget sodales vitae,
-        rhoncus eget sapien:</p>
-        <ol>
-          <li>Nulla pulvinar diam</li>
-          <li>Facilisis bibendum</li>
-          <li>Vestibulum vulputate</li>
-          <li>Eget erat</li>
-          <li>Id porttitor</li>
-        </ol>
+        <h2>Team Members</h2>
       </div>
     );
   }
